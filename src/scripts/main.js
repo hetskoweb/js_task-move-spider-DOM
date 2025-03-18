0e9666f26170008cde29fb476eeafdc0544283c6
Illia <hetsko.ilya@gmail.com>
2025-03-18 17:20:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
   // write code here
+  const wallSize = wall.getBoundingClientRect();
+  const spiderSize = spider.getBoundingClientRect();
+
+  const x = e.clientX - wallSize.left - spiderSize.width / 2;
+  const y = e.clientY - wallSize.top - spiderSize.height / 2;
+
+  spider.style.position = 'absolute';
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
 });
